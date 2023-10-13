39ecab4a6bdb3f1c2e56b85119b5b656e6c425cd
Kamilla012 <93913165+Kamilla012@users.noreply.github.com>
2023-10-13 15:36:39 +0200

    post

--- my-app/src/pages/RegisterPage.js
+++ my-app/src/pages/RegisterPage.js
@@ -1,13 +1,35 @@
 import styles from "../style"
+import { useState } from "react";
 
 export default function RegisterPage(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    async function register(ev){
+        ev.preventDefault();
+        await fetch('http://localhost:4000/register', {
+            method: 'POST'  ,
+            body: JSON.stringify({username, password}),
+            header: {'Content-Type':'application/js'}
+        })
+    }
     return(
         <div className={`${styles.divForm}`}>
 
-            <form className={`${styles.form}`}>
+            <form onSubmit={register} className={`${styles.form}`} >
                 <h2 className={`${styles.h2}`}>Register</h2>
-                <input type="text" placeholder="username" className={`${styles.inputForm}`} />
-                <input type="password" placeholder="password" className={`${styles.inputForm}`} />
+
+                <input type="text"
+                    placeholder="username"
+                    value={username}
+                    onChange={ev => setUsername(ev.target.value)}
+                    className={`${styles.inputForm}`} />
+
+                <input type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={ev => setPassword(ev.target.value)}
+                    className={`${styles.inputForm}`} />
+
                 <button className={styles.buttonForm}>Register</button>
             </form>
         </div>
