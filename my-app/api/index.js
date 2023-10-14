9768617ac951f652d26fa476371f89ed1f93618b
Kamilla012 <93913165+Kamilla012@users.noreply.github.com>
2023-10-14 11:54:23 +0200

    update Register

--- my-app/api/index.js
+++ my-app/api/index.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 
+const User = require('./models/User');
+app.use(express.json());
 app.use(cors());
-app.use(express.json()) 
 
-app.post('/register', (req, res) =>{
-    const {username, password} = req.body;
-    res.json({requestData:{username,password}});
-});
+
+
+mongoose.connect('mongodb+srv://admin:i5ri1fNyrrImBiDp@cluster0.8vrqt6j.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+      const userDoc = await User.create({ username, password });
+      res.json(userDoc);
+    } catch (error) {
+      if (error.code === 11000) {
+        // Błąd duplikatu klucza
+        res.status(400).json({ error: 'Użytkownik o tej nazwie już istnieje.' });
+      } else {
+        res.status(500).json({ error: 'Wystąpił błąd serwera.' });
+      }
+    }
+  });
+  
 
 app.listen(4000)
--- my-app/src/pages/RegisterPage.js
+++ my-app/src/pages/RegisterPage.js
@@ -4,13 +4,13 @@ import { useState } from "react";
 export default function RegisterPage(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    async function register(ev){
+    async function register(ev) {
         ev.preventDefault();
-        await fetch('http://localhost:4000/register', {
-            method: 'POST'  ,
-            body: JSON.stringify({username, password}),
-            header: {'Content-Type':'application/js'}
-        })
+        const response = await fetch('http://localhost:4000/register', {
+          method: 'POST',
+          body: JSON.stringify({username,password}),
+          headers: {'Content-Type':'application/json'},
+        });
     }
     return(
         <div className={`${styles.divForm}`}>
