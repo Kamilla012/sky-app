9635fa1a5ca4b7ce20baaa42a06f678b18127e4a
Kamilla012 <93913165+Kamilla012@users.noreply.github.com>
2023-11-09 21:36:25 +0100

    technologies.js

--- my-app/src/components/Technologies.js
+++ my-app/src/components/Technologies.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact, faNode, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons';
 
 const Technologies = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
 
-export default Technologies
+  const iconNames = [faReact, faNode, faDatabase, faGithub];
+
+  return (
+    <div className='flex justify-center w'>
+
+      {iconNames.map((icon, index) => (
+        <FontAwesomeIcon key={index} icon={icon} className='text-white  p-10 m-5 text-[70px]' />
+      ))}
+    </div>
+  );
+};
+
+export default Technologies;
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import SateliteInfo from '../components/SateliteInfo'
 import { StarsInfo } from '../components/StarsInfo'
+import Technologies from '../components/Technologies'
 
 const Home = () => {
   return (
     <div className='bg-primary'>
     <div><SateliteInfo /></div>
+    <Technologies />
     <div><StarsInfo/></div>
     </div>
   )
