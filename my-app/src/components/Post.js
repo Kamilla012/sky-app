3ad4eb32b35574b5a9c3f0eed77e70068986e7a4
Kamilla012 <93913165+Kamilla012@users.noreply.github.com>
2023-12-13 09:17:29 +0100

    blog, add like

--- my-app/src/components/Post.js
+++ my-app/src/components/Post.js
@@ -1,35 +1,57 @@
-import { format, formatISO9075 } from "date-fns";
-import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { format } from "date-fns";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import {io} from 'socket.io-client'
+import { io } from "socket.io-client";
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
-
-const socket = io('/',{
+const socket = io.connect("http://localhost:4001/", {
   reconnection: true
+});
 
-})
+export default function Post({
+  _id,
+  title,
+  summary,
+  cover,
+  content,
+  createdAt,
+  author,
+}) {
+  const [isHeartRed, setHeartRed] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
 
-export default function Post({_id, title, summary, cover, content, createdAt, author }) {
-  // Tworzymy pełną ścieżkę do obrazu, łącząc ścieżkę frontendu z nazwą pliku obrazu
+  const handleClick = () => {
+    setHeartRed(!isHeartRed);
+    const newLikeCount = isHeartRed ? likeCount - 1 : likeCount + 1;
+    setLikeCount(newLikeCount);
 
-  useEffect(()=>{
-    console.log("SOCKET IO", socket)
-  },[])
+    // Emitowanie zdarzenia do serwera socket.io
+    socket.emit('likeAdded', { postId: _id, isLiked: !isHeartRed });
+  };
+
+  useEffect(() => {
+    // Odbieranie zdarzenia od serwera socket.io
+    socket.on('likeUpdate', (data) => {
+      if (data.postId === _id) {
+        // setLikeCount(data.isLiked ? likeCount + 1 : likeCount - 1);
+      }
+    });
+
+    // Odczyt zdarzenia po odmontowaniu komponentu
+    return () => {
+      socket.off('likeUpdate');
+    };
+  }, [_id, likeCount]);
   return (
     <div className="w-[50%] text-[white] mt-10 my-5 grid-cols-2 gap-20">
-
-    
-      
-
       <div>
         <Link to={`/post/${_id}`}>
-            <h2 className="text-[30px]">{title}</h2>
+          <h2 className="text-[30px]">{title}</h2>
         </Link>
 
-
         <p className="mb-5 mt-2 text-[green] ">
-
-        <time className="text-[grey]">
+          <time className="text-[grey]">
             {format(new Date(createdAt), "d MMM yyyy HH:mm")}
           </time>
 
@@ -38,14 +60,24 @@ export default function Post({_id, title, summary, cover, content, createdAt, au
           </a>
         </p>
         <Link to={`/post/${_id}`}>
-        <img src={'http://localhost:4000/'+cover} alt="post_img"/>
-    </Link>
-  
+          <img src={"http://localhost:4000/" + cover} alt="post_img" />
+        </Link>
+
         <p className="text-[16px]">{summary}</p>
- 
-        <div className="text-white mt-6" dangerouslySetInnerHTML={{__html:content}} />
+
+        <div
+          className="text-white mt-6"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
       </div>
+      <FontAwesomeIcon
+        icon={faHeart}
+        className="text-[30px]"
+        style={{ color: isHeartRed ? 'red' : 'white' }}
+        onClick={handleClick}
+      />
+      <p className="text-[16px]">Likes: {likeCount}</p>
     </div>
+   
   );
 }
-
